9d23e89b742efe797c65ab0709de2e759b4aca36
kasmith <k2smith@mit.edu>
2022-08-17 15:14:47 -0400

    Added VertexDrawer interface (draft)

--- virtualtools/interfaces/__init__.py
+++ virtualtools/interfaces/__init__.py
@@ -1,5 +1,6 @@
 from .running import run_game, get_path, get_state_path, get_geom_path, get_collisions, CollisionError
 from .vtinterface import VTInterface, check_collision_by_polys, place_object_by_polys, VTActionError
 from .toolpicker import ToolPicker, load_tool_picker
+from .vertexdrawer import VertexDrawer
 
-__all__ = ['ToolPicker', 'load_tool_picker']
+__all__ = ['ToolPicker', 'load_tool_picker', 'VertexDrawer']
---
+++ virtualtools/interfaces/vertexdrawer.py
@@ -0,0 +1,34 @@
+from typing import Tuple, Annotated, Dict
+from .vtinterface import VTInterface, place_object_by_vertex_list
+from geometry import check_counterclockwise
+
+__all__ = ['VertexDrawer']
+
+class VertexDrawer(VTInterface):
+    def __init__(self,
+                 worlddict: Dict,
+                 basic_timestep: float=0.1,
+                 maxtime: float=20.,
+                 world_timestep: float=None):
+        super().__init__(worlddict, basic_timestep, maxtime, world_timestep)
+
+    @property
+    def action_keys(self):
+        return ['vertexlist', 'position']
+
+    @property
+    def interface_type(self):
+        return "VertexDrawer"
+
+    def place(self,
+              action: Dict,
+              world: VTWorld=None) -> VTWorld:
+        world = world or load_vt_from_dict(self._worlddict)
+        self._check_action(action)
+        vertices = action['vertexlist']
+        pos = action['position']
+        if not check_counterclockwise(vertices):
+            vertices.reverse()
+            assert not check_counterclockwise(vertices), "Bad vertex definition: no consistent winding"
+        world = place_object_by_vertex_list(world, vertices, pos)
+        return world
