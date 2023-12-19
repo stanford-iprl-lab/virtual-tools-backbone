728da105d44fa65aed92d44824eebdd64c601fe9
kasmith <k2smith@mit.edu>
2023-12-19 13:31:02 -0500

    Added lure function, docstrings

--- virtualtools/generators/vtgenerator.py
+++ virtualtools/generators/vtgenerator.py
@@ -3,22 +3,26 @@ from ..interfaces import VTInterface
 from ..world import VTWorld
 from abc import ABC, abstractmethod
 import copy, random
+import warnings
 
 __all__ = ['VTGenerator', 'StaticGenerator']
 
-
-"""
-Keyword arguments:
-argument -- description
-Return: return_description
-"""
-
 class VTGenerator(ABC):
+    """An abstract class used as the basis for level generators
+    
+    See https://docs.google.com/document/d/1tmdCncGLO2KA2Y7aYs5WW07VNhIiKHvu8CAJ1pkZBEY/edit?usp=sharing for more details
+    """
     
     _opts = {}
     
     def __init__(self, options: Dict):
-        # Initialize arguments
+        """Initialize a VTGenerator. Though this is abstract, most inheritors won't need to overwrite this
+        
+        The `options` dictionary can have the following items 
+
+        Args:
+            options (Dict): A dictionary with the following 
+        """        
         self.set_options(**options)
     
     # Method to be overwritten that creates the world (specific to each interface)
@@ -33,10 +37,24 @@ class VTGenerator(ABC):
     def random_specific_action(self, interface: VTInterface) -> Dict:
         raise NotImplementedError("Must overwrite random_specific_action method in inheritor if specific constraints are set")
     
+    def random_lure_action(self, interface: VTInterface) -> Dict:
+        raise NotImplementedError("Must overwrite random_lure_action method in inheritor if lure constraints are set")
+    
+    
     def generate_world(self,
-                       maxiters: int = 10000,
+                       maxiters: int = 1000,
                        seed: int = None,
                        verbose: bool = False) -> VTInterface:
+        """Runs the generator to produce a world within the given constraints
+
+        Args:
+            maxiters (int, optional): Total number of failed world creation attempts before giving up and returning None. Defaults to 1000.
+            seed (int, optional): The random seed to set for consistent generation. Defaults to None.
+            verbose (bool, optional): A flag if detailed output should be written out. Defaults to False.
+
+        Returns:
+            VTInterface: A random level proposed by `propose_world` and passing all requested checks. Or None if no level can be found
+        """        
         if seed:
             random.seed(seed)
         # Loop until a world is generated or it times out
@@ -61,10 +79,19 @@ class VTGenerator(ABC):
     
     # Returns a boolean specifying if the randomly generated actions are within the option bounds    
     def check_placements(self, interface: VTInterface, verbose: bool = False) -> bool:
+        """A function to determine whether a given world/interface passes the requested checks
+
+        Args:
+            interface (VTInterface): The VTInterface often proposed by `propose_world`
+            verbose (bool, optional): A flag if detailed output should be written out. Defaults to False.
+
+        Returns:
+            bool: Returns True if all the set checks are passed, False if any are failed
+        """        
         minap = self._opts['min_any_place']
         maxap = self._opts['max_any_place']
         if minap > 0.0 and maxap < 1.0:
-            anyprop = self._check_any_placement(interface)
+            anyprop = self._check_placement(interface, 'specific')
             if verbose:
                 print('Proportion of any random successes:', anyprop)
             if anyprop < minap:
@@ -79,7 +106,7 @@ class VTGenerator(ABC):
         minsp = self._opts['min_spec_place']
         maxsp = self._opts['max_spec_place']
         if minsp > 0.0 and maxsp < 1.0:
-            specprop = self._check_specific_placement(interface)
+            specprop = self._check_placement(interface, 'specific')
             if verbose:
                 print('Proportion of specific random successes:', specprop)
             if specprop < minsp:
@@ -91,6 +118,21 @@ class VTGenerator(ABC):
                     print('Failed: specific random placement too high')
                 return False
             
+        minlp = self._opts['min_lure_place']
+        maxlp = self._opts['max_lure_place']
+        if minlp > 0.0 and maxlp < 1.0:
+            lureprop = self._check_placement(interface, 'lure')
+            if verbose:
+                print('Proportion of lure random successes:', lureprop)
+            if lureprop < minlp:
+                if verbose:
+                    print('Failed: lure random placement too low')
+                return False
+            if lureprop > maxlp:
+                if verbose:
+                    print('Failed: lure random placement too high')
+                return False
+            
         if verbose:
             print("Passed all random action checks")
         return True
@@ -100,28 +142,23 @@ class VTGenerator(ABC):
     def generate_random_placement(self, interface: VTInterface) -> Dict:
         raise NotImplementedError('This should be implemented by an interface generator')
     
-    # Method that returns the proportion of random actions that are successful
-    def _check_any_placement(self, interface: VTInterface) -> float:
+    # Helper method to get the number of successes from a random level
+    def _check_placement(self, interface: VTInterface, plc_type: str = "any"):
+        if plc_type == 'any':
+            fn = self.generate_random_placement
+        elif plc_type == 'specific':
+            fn = self.random_specific_action
+        elif plc_type == 'lure':
+            fn = self.random_lure_action
+        else:
+            raise ValueError("plc_type must be one of 'any', 'specific', or 'lure'")
         numsuc = 0
         for _ in range(self._opts['nsims']):
-            act = self.generate_random_placement(interface)
+            act = fn(interface)
             r = None
             # Ignore failed placements
             while r is None:
-                act = self.generate_random_placement(interface)
-                r = interface.run_placement(act)[0]
-            if r:
-                numsuc += 1
-        return numsuc / self._opts['nsims']
-    
-    # Method returning proportion of actions defined in random_specific_action that are successful
-    def _check_specific_placement(self, interface: VTInterface) -> float:
-        numsuc = 0
-        for _ in range(self._opts['nsims']):
-            act = self.random_specific_action(interface)
-            r = None
-            while r is None:
-                act = self.random_specific_action(interface)
+                act = fn(interface)
                 r = interface.run_placement(act)[0]
             if r:
                 numsuc += 1
@@ -136,6 +173,8 @@ class VTGenerator(ABC):
         _set_opt('max_any_place', 1.0)
         _set_opt('min_spec_place', 0.0)
         _set_opt('max_spec_place', 1.0)
+        _set_opt('min_lure_place', 0.0)
+        _set_opt('max_lure_place', 1.0)
         _set_opt('nsims', 100)
         
     @property
@@ -146,12 +185,36 @@ class VTGenerator(ABC):
 # A stub abstract class for generating worlds without randomness
 # Note that this is typically only used 
 class StaticGenerator(VTGenerator):
+    """A class to inheret from in order to generate world with no randomness
+    """    
     
     def generate_random_placement(self, interface: VTInterface) -> Dict:
         raise NotImplementedError('This function should never be called!')
         
     def set_options(self, **kwargs):
-        badkws = ['min_any_place', 'max_any_place', 'min_spec_place', 'max_spec_place']
+        badkws = ['min_any_place', 'max_any_place', 'min_spec_place', 'max_spec_place',
+                  'min_lure_place', 'max_lure_place']
         if any([kw in kwargs.keys() for kw in badkws]):
             raise Exception('Cannot set acceptability ranges for StaticGenerators')
         return super().set_options(**kwargs)
+    
+    def generate_world(self,
+                       maxiters: int = 1000,
+                       seed: int = None,
+                       verbose: bool = False) -> VTInterface:
+        """Runs the generator to produce a world
+
+        Args:
+            maxiters (int, optional): Ignored; only used for inheritance. Defaults to 1000.
+            seed (int, optional): The random seed to set for consistent generation. Defaults to None.
+            verbose (bool, optional): A flag if detailed output should be written out. Defaults to False.
+
+        Returns:
+            VTInterface: A level proposed by `propose_world`
+        """        
+        if seed:
+            random.seed(seed)
+            
+        if maxiters != 1000 or (verbose is not False):
+            warnings.Warn("Note that maxiters and verbose options are unused for static generators")
+        return self.propose_world()
